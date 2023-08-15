ebb4c94a2f183a1be24a7d97c486e8eb69e46400
Lucas Beneti <lbeneti@gmail.com>
2023-08-15 20:20:05 -0300

    Added radix-ui scrollArea

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,84 @@
+import { NavLink } from 'react-router-dom';
 import { RecentPlayedCard } from '../components/RecentPlayedCard';
-
+import { RecentPodcast } from '../components/RecentPodcast';
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 export const Home = () => {
+    const testPodcasts = [
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+        {
+            title: 'Grizzly Peaks Radio',
+            subtitle: 'Andy Goodman',
+            coverSrc:
+                'https://i.scdn.co/image/850f5a2b54854f56f4f3b01179b9ce84b32ef3b2',
+        },
+    ];
     return (
-        <section>
+        <section className='flex flex-col gap-y-6 p-6 h-full'>
             <h1 className='text-4xl font-bold'>Boa noite</h1>
             <section className='flex flex-1 flex-wrap gap-x-6 gap-y-4 mt-4'>
                 <RecentPlayedCard
@@ -30,6 +106,41 @@ export const Home = () => {
                     name='Liked songs'
                     imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
                 />
+            </section>
+            <section className='mt-4'>
+                <header className='flex justify-between items-center'>
+                    <NavLink to='/'>
+                        <h2 className='hover:underline text-3xl font-bold mb-4'>
+                            Seus Programas
+                        </h2>
+                    </NavLink>
+                    <NavLink to='/'>
+                        <p className='font-bold text-base text-white hover:underline'>
+                            Mostrar tudo
+                        </p>
+                    </NavLink>
+                </header>
+                <ScrollArea.Root>
+                    <ScrollArea.Viewport>
+                        <section className='flex flex-wrap gap-6 h-80'>
+                            {testPodcasts.map((p) => {
+                                return (
+                                    <RecentPodcast
+                                        title={p.title}
+                                        subtitle={p.subtitle}
+                                        coverSrc={p.coverSrc}
+                                    />
+                                );
+                            })}
+                        </section>
+                    </ScrollArea.Viewport>
+                    <ScrollArea.Scrollbar orientation='vertical'>
+                        <ScrollArea.Thumb />
+                    </ScrollArea.Scrollbar>
+                    <ScrollArea.Scrollbar orientation='horizontal'>
+                        <ScrollArea.Thumb />
+                    </ScrollArea.Scrollbar>
+                </ScrollArea.Root>
             </section>
         </section>
     );
