23ac82aef844046ecb203ca9706c412074745bfe
Lucas Beneti <lbeneti@gmail.com>
2023-08-15 17:51:20 -0300

    added a couple more styling for the recently played cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { RecentPlayedCard } from './components/RecentPlayedCard';
 function App() {
     return (
         <>
-            <div className='flex bg-black flex-col text-white'>
+            <div className='flex bg-black flex-col text-white h-screen'>
                 <div className='flex'>
                     <Sidebar />
                     <main className='flex-1 bg-base my-2 p-2 rounded-md '>
@@ -18,8 +18,9 @@ function App() {
                         </div>
                         <section>
                             <h1 className='text-4xl font-bold'>Boa noite</h1>
-                            <section className='flex flex-1 flex-wrap'>
+                            <section className='flex flex-1 flex-wrap gap-x-6 gap-y-4 mt-4'>
                                 <RecentPlayedCard
+                                    currentlyPlaying
                                     name='Liked songs'
                                     imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
                                 />
@@ -31,7 +32,14 @@ function App() {
                                     name='Liked songs'
                                     imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
                                 />
-
+                                <RecentPlayedCard
+                                    name='Liked songs'
+                                    imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
+                                />
+                                <RecentPlayedCard
+                                    name='Liked songs'
+                                    imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
+                                />
                                 <RecentPlayedCard
                                     name='Liked songs'
                                     imgSrc='https://images.unsplash.com/photo-1513104487127-813ea879b8da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2746&q=80'
--- src/components/RecentPlayedCard.tsx
+++ src/components/RecentPlayedCard.tsx
@@ -1,12 +1,37 @@
-interface RecentPlayedCardProps {
+import { useState } from 'react';
+import { Play, Pause } from '@phosphor-icons/react';
+type RecentPlayedCardProps = {
     name: string;
     imgSrc: string;
-}
-export const RecentPlayedCard = ({ name, imgSrc }: RecentPlayedCardProps) => {
+    currentlyPlaying?: boolean;
+};
+export const RecentPlayedCard = ({
+    name,
+    imgSrc,
+    currentlyPlaying,
+}: RecentPlayedCardProps) => {
+    const [playing, setPlaying] = useState<boolean>(true);
+    const handlePlayingToggle = () => {
+        setPlaying(!playing);
+    };
     return (
-        <div className='flex rounded-lg bg-highlight w-4/12 m-2'>
-            <img className='w-20 h-auto' src={imgSrc} alt='' />
-            <h2 className='text-2xl font-bold'>{name}</h2>
+        <div className='flex justify-between rounded-lg bg-highlight w-[30%] h-20'>
+            <div className='flex gap-x-4 items-center'>
+                <img className='w-20 h-20 object-cover' src={imgSrc} alt='' />
+                <h2 className='text-2xl font-bold'>{name}</h2>
+            </div>
+            {currentlyPlaying ? (
+                <button
+                    onClick={handlePlayingToggle}
+                    className='self-center justify-end bg-primary p-2 mr-4 rounded-full transition hover:scale-105'
+                >
+                    {playing ? (
+                        <Pause size={24} weight='fill' fill='black' />
+                    ) : (
+                        <Play size={24} weight='fill' fill='black' />
+                    )}
+                </button>
+            ) : null}
         </div>
     );
 };
