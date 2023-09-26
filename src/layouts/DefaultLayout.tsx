df232cb414f6aff3915474c3daca9ed3e4eca9b2
Lucas Beneti <lbeneti@gmail.com>
2023-09-26 09:03:13 -0300

    implemented header banner for the ArtistDetail component

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -15,9 +15,9 @@ export const DefaultLayout = () => {
                 <div className='flex bg-black flex-col text-white h-screen'>
                     <div className='flex gap-x-2 p-2'>
                         <Sidebar />
-                        <main className='flex flex-col flex-1 bg-base p-2 rounded-md h-[calc(100vh-5.5rem)] overflow-hidden relative'>
+                        <main className='flex flex-col flex-1 bg-base rounded-md h-[calc(100vh-5.5rem)] overflow-hidden relative'>
                             <header
-                                className='flex flex-1 w-full absolute top-0 justify-between pr-4 pt-4'
+                                className='flex flex-1 w-full absolute top-0 justify-between pr-4 pl-4 pt-4'
                                 style={{ zIndex: 100 }}
                             >
                                 <div className='flex gap-x-2'>
--- src/pages/ArtistDetail.tsx
+++ src/pages/ArtistDetail.tsx
@@ -3,15 +3,21 @@ export const ArtistDetail = () => {
     const { artistId } = useParams();
     return (
         <>
-            <header className='h-64 w-full bg-highlight absolute top-2 '>
-                <img
-                    src='https://images.unsplash.com/photo-1549834125-82d3c48159a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmFuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'
-                    alt={`${artistId} Banner`}
-                    className='h-72 w-full object-cover overflow-hidden rounded'
-                />
-                <h1 className='text-8xl font-bold font-display'>{artistId}</h1>
+            <header className='h-64 w-full bg-highlight'>
+                <span className='w-full border-t-md'>
+                    <img
+                        src='https://images.unsplash.com/photo-1549834125-82d3c48159a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmFuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'
+                        alt={`${artistId} Banner`}
+                        className='h-72 w-full object-cover overflow-hidden rounded'
+                    />
+                </span>
+                <h1 className='text-8xl font-bold font-display absolute top-48 px-4'>
+                    {artistId}
+                </h1>
             </header>
-            <h1>Astist {artistId}</h1>
+            <main className='mt-16'>
+                <h1>Astist {artistId}</h1>
+            </main>
         </>
     );
 };
