8f6a8f24f01dd9d3a1e604f8cd61cd60c854c19e
Lucas Beneti <lbeneti@gmail.com>
2023-10-04 09:14:43 -0300

    added auth using Clerk <3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { ClerkProvider } from '@clerk/clerk-react';
 import { Router } from './Router';
+
+if (!import.meta.env.VITE_CLERK_PUBLISHABLE_KEY) {
+    throw new Error('missing publishable key');
+}
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 function App() {
-    return <Router />;
+    return (
+        <ClerkProvider publishableKey={clerkPubKey}>
+            <Router />
+        </ClerkProvider>
+    );
 }
 
 export default App;
