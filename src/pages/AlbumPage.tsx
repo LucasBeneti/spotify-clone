8db9dd134f1540e9045ea76e0a39627b84940d93
lucasbeneti <lbeneti@gmail.com>
2023-11-21 01:27:03 -0300

    minor css fix on AlbumPage, still need some work

--- src/pages/AlbumPage.tsx
+++ src/pages/AlbumPage.tsx
@@ -90,91 +90,93 @@ export const AlbumPage = () => {
   // TODO fix the album vertical overflow
   return (
     <>
-      <header className="w-100 bg-gradient-to-b from-cyan-950 to-base h-96">
-        <section className="flex gap-x-4 items-end mt-24 px-6">
-          <img
-            src={albumSongData?.cover_art}
-            alt="playlist cover art"
-            className="w-48"
-          />
-          <div>
-            <p className="text-xs">Playlist</p>
-            <h3 className="sm:text-lg md:text-2xl xl:text-5xl font-display font-bold mt-3 mb-6">
-              {albumSongData?.name}
-            </h3>
-            <p className="text-xs font-bold">{albumSongData?.author_name}</p>
-          </div>
-        </section>
-      </header>
-      <main className="w-full px-6 -translate-y-24 bg-base bg-opacity-10">
-        <section className="flex gap-x-8 items-center my-8">
-          <BigPlayButton
-            onClickHandle={() => {
-              console.log("play this playlist");
-            }}
-          />
-          <button
-            onClick={handleLikeAlbum}
-            className="hover:scale-105 transition-all delay-75"
-          >
-            <Heart fill="#1ed760" weight={"regular"} size={32} />
-          </button>
-        </section>
-        <section className="overflow-auto">
-          <table className="table-auto border-collapse bg-transparent w-full">
-            <thead className="my-2 border-b-neutral-800 border-b">
-              <tr>
-                <th className="p-4 text-center text-white">#</th>
-                <th className="p-4 text-left text-white">Title</th>
-                <th className="p-4 text-left text-white">
-                  <Clock size={24} fill="#fff" />
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {isLoading ? (
+      <div className="overflow-y-auto">
+        <header className="w-100 bg-gradient-to-b from-cyan-950 to-base ">
+          <section className="flex gap-x-4 items-end mt-24 px-6">
+            <img
+              src={albumSongData?.cover_art}
+              alt="playlist cover art"
+              className="w-56 hover:scale-105 delay-100 transition-all"
+            />
+            <div>
+              <p className="text-xs">Album</p>
+              <h3 className="sm:text-lg md:text-2xl xl:text-5xl font-display font-bold mt-3 mb-6">
+                {albumSongData?.name}
+              </h3>
+              <p className="text-xs font-bold">{albumSongData?.author_name}</p>
+            </div>
+          </section>
+        </header>
+        <main className="w-full h-full px-6  bg-base bg-opacity-10">
+          <section className="flex gap-x-8 items-center my-8">
+            <BigPlayButton
+              onClickHandle={() => {
+                console.log("play this playlist");
+              }}
+            />
+            <button
+              onClick={handleLikeAlbum}
+              className="hover:scale-105 transition-all delay-75"
+            >
+              <Heart fill="#1ed760" weight={"regular"} size={32} />
+            </button>
+          </section>
+          <section className="overflow-auto">
+            <table className="table-auto border-collapse bg-transparent w-full">
+              <thead className="my-2 border-b-neutral-800 border-b">
                 <tr>
-                  <td>
-                    <h2>Loading...</h2>
-                  </td>
+                  <th className="p-4 text-center text-white">#</th>
+                  <th className="p-4 text-left text-white">Title</th>
+                  <th className="p-4 text-left text-white">
+                    <Clock size={24} fill="#fff" />
+                  </th>
                 </tr>
-              ) : (
-                albumSongData?.songs?.map((song, index) => {
-                  return (
-                    <tr
-                      className="group/item hover:bg-highlight transition cursor-pointer"
-                      onClick={() => handlePlayThis(song)}
-                      key={`${song}_${index}`}
-                    >
-                      <td className="text-sm p-4 text-white ">
-                        <span className="flex justify-center items-center">
-                          <span className="block group-hover/item:hidden px-2">
-                            {index + 1}
+              </thead>
+              <tbody>
+                {isLoading ? (
+                  <tr>
+                    <td>
+                      <h2>Loading...</h2>
+                    </td>
+                  </tr>
+                ) : (
+                  albumSongData?.songs?.map((song, index) => {
+                    return (
+                      <tr
+                        className="group/item hover:bg-highlight transition cursor-pointer"
+                        onClick={() => handlePlayThis(song)}
+                        key={`${song}_${index}`}
+                      >
+                        <td className="text-sm p-4 text-white ">
+                          <span className="flex justify-center items-center">
+                            <span className="block group-hover/item:hidden px-2">
+                              {index + 1}
+                            </span>
+                            <span className="hidden group-hover/item:block">
+                              <Play size={14} weight="fill" fill="white" />
+                            </span>
                           </span>
-                          <span className="hidden group-hover/item:block">
-                            <Play size={14} weight="fill" fill="white" />
-                          </span>
-                        </span>
-                      </td>
-                      <td className="text-sm p-4 text-left text-white">
-                        <SongItem
-                          artist={song.artist_name}
-                          name={song.name}
-                          imgSrc={song.cover_art}
-                          variant="playlist"
-                        />
-                      </td>
-                      <td className="p-4 text-left text-white">
-                        {getSongDurationInMinutes(song.duration)}
-                      </td>
-                    </tr>
-                  );
-                })
-              )}
-            </tbody>
-          </table>
-        </section>
-      </main>
+                        </td>
+                        <td className="text-sm p-4 text-left text-white">
+                          <SongItem
+                            artist={song.artist_name}
+                            name={song.name}
+                            imgSrc={song.cover_art}
+                            variant="playlist"
+                          />
+                        </td>
+                        <td className="p-4 text-left text-white">
+                          {getSongDurationInMinutes(song.duration)}
+                        </td>
+                      </tr>
+                    );
+                  })
+                )}
+              </tbody>
+            </table>
+          </section>
+        </main>
+      </div>
     </>
   );
 };
